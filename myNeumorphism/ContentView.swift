bcb72f8b14f8e24f8b020add35db353decad0a35
Brian Malagoli <brianmb@gmx.com>
2020-05-05 14:13:09 +0200

    Initial Commit

---
+++ myNeumorphism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myNeumorphism
+//
+//  Created by Brian Malagoli on 05/05/2020.
+//  Copyright © 2020 Brian Malagoli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
